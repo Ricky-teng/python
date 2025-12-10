ec1471ed5e94fcd4a9b9751103bdab4df335d2d7
Ricky <tengchihchi@gmail.com>
2025-12-10 10:34:56 +0800

    Create 90214.py

---
+++ python/90214.py
@@ -0,0 +1,11 @@
+x = int(input('請輸入個人國文成績:'))
+= int(input('請輸入個人英文成績:')) y
+z= int(input('請輸入個人數學成積:'))
+a = (x + y + z) / 3
+b = (70+75 +65) / 3
+print('個人平均成績是',a)
+if a > b:
+print('你很棒。')
+elif ab:
+print('多多加油。)
+else: print('繼續保持。')
